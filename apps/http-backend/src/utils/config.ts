aaaf7522ea2a4a666cb6ec0ad37edc5fea9a1c77
ritik6559 <ritikjoshi741@gmail.com>
2026-04-01 21:57:04 +0530

    feat: refactor mail configuration to use constants from config module

--- apps/http-backend/src/utils/config.ts
+++ apps/http-backend/src/utils/config.ts
@@ -1,5 +1,13 @@
+// AUTH CONFIG
 export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET!;
 export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET!;
 export const ACCESS_TOKEN_EXPIRY = process.env.ACCESS_TOKEN_EXPIRY || "15m";
 export const REFRESH_TOKEN_EXPIRY = process.env.REFRESH_TOKEN_EXPIRY || "7d";
 export const OTP_SALT = process.env.OTP_SALT!;
+
+// MAIL CONFIG
+export const SMTP_HOST = process.env.SMTP_HOST!;
+export const SMTP_PORT = Number(process.env.SMTP_PORT) || 465;
+export const SMTP_SERVICE = process.env.SMTP_SERVICE!;
+export const SMTP_USER = process.env.SMTP_USER!;
+export const SMTP_PASS = process.env.SMTP_PASS!;
--- apps/http-backend/src/utils/send-mail/index.ts
+++ apps/http-backend/src/utils/send-mail/index.ts
@@ -2,16 +2,17 @@ import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
 import ejs from "ejs";
 import path from "path";
+import { SMTP_HOST, SMTP_PASS, SMTP_PORT, SMTP_SERVICE, SMTP_USER } from '../config';
 
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: Number(process.env.SMTP_PORT) || 587,
-    service: process.env.SMTP_SERVICE,
+    host: SMTP_HOST,
+    port: SMTP_PORT,
+    service: SMTP_SERVICE,
     auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS
+        user: SMTP_USER,
+        pass: SMTP_PASS
     },
 });
 
